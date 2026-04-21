2531a442f9b6da4b80540290f6060aa8ac6c7cd5
nurickan <148078275+nurickan@users.noreply.github.com>
2026-04-22 02:11:25 +0500

    build(ui): add webpack bundle and dev server config

---
+++ web/webpack.config.js
@@ -0,0 +1,2 @@
+const path = require("path"); const HtmlPlugin = require("html-webpack-plugin")
+module.exports = { entry: "./src/main.tsx", output: { path: path.resolve(__dirname, "dist"), filename: "bundle.js", clean: true }, resolve: { extensions: [".ts", ".tsx", ".js", ".jsx"] }, module: { rules: [{ test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }] }, plugins: [new HtmlPlugin({ template: "./src/index.html" })], devServer: { static: [{ directory: path.join(__dirname, "public") }], port: 3000 } }
